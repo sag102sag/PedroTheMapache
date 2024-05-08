ea3216010b215947c3511949a78f5277947ec56e
windows10 <windows10@DESKTOP-B8E4IF5>
2024-05-08 10:39:06 +0200

    primer commit

---
+++ Ejemplo1Git/src/calculadora/Calculator.java
@@ -0,0 +1,25 @@
+package calculadora;
+public class Calculator {
+	 public double add(double number1, double number2){
+	 return number1 + number2;
+	 }
+	 public double subtract(double number1, double number2){
+	 return number1 - number2;
+	 }
+	 public double multiply(double number1, double number2) {
+	 return number1 * number2;
+	 }
+	 public double divide(double number1, double number2){
+	 return number1 / number2;
+	 }
+	 public boolean esMayor (double number1, double number2)
+	 {
+		 return number1>number2;
+	 }
+	 public boolean esMenor (double number1, double number2)
+	 {
+		 return number1<number2;
+	 }
+	 
+	 
+	}
