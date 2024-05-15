88048fcbacdf234213b0238d58d71dbb198bb28a
windows10 <windows10@DESKTOP-B8E4IF5>
2024-05-15 10:28:01 +0200

    add --> sumar

--- Ejemplo1Git/src/calculadora/Calculator.java
+++ Ejemplo1Git/src/calculadora/Calculator.java
@@ -1,6 +1,6 @@
 package calculadora;
 public class Calculator {
-	 public double add(double number1, double number2){
+	 public double sumar(double number1, double number2){
 	 return number1 + number2;
 	 }
 	 public double subtract(double number1, double number2){
